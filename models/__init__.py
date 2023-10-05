1a6e262de974b5fe281898944cca048849315e1e
KevinSimo2022 <k.takoulo@alustudent.com>
2023-10-05 08:26:33 +0200

    Revert "done"
    This reverts commit e888b0b98a0e4b0489223df6b5b47ea545a9c431, reversing
    changes made to 38f816ea057f374a08ab581a46e824fa6f68712f.

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """Instantiates a storage object.
 """
 from os import getenv
@@ -12,19 +11,3 @@ else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
-=======
-"""This module instantiates an object of class FileStorage"""
-import os
-
-if os.getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
-
-    storage = DBStorage()
-    storage.reload()
-else:
-
-    from models.engine.file_storage import FileStorage
-
-    storage = FileStorage()
-    storage.reload()
->>>>>>> 4522ce7d33db649bb27d03c26fe635d9c6886bb8
