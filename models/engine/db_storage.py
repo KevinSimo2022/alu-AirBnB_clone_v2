1355da4b7e166d5ba75547935c6b1dd9b57b9ad3
KevinSimo2022 <k.takoulo@alustudent.com>
2023-10-05 13:45:55 +0200

    Revert "done"
    This reverts commit d85b3d089a76b0f0873314d931136fd9316a9c75.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -12,6 +12,7 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 class DBStorage:
     """Represents a database storage engine.
     Attributes:
@@ -19,23 +20,24 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Query on the curret database session all objects of the given class.
+        """Query on the current database session all objects of the given class.
         If cls is None, queries all types of objects.
         Return:
             Dict of queried classes in the format <class name>.<obj id> = obj.
         """
         if cls is None:
-            objs = self.__session.query(State).all()
-            objs.extend(self.__session.query(City).all())
-            objs.extend(self.__session.query(User).all())
-            objs.extend(self.__session.query(Place).all())
-            objs.extend(self.__session.query(Review).all())
-            objs.extend(self.__session.query(Amenity).all())
+            classes = [State, City, User, Place, Review, Amenity]
+        elif isinstance(cls, str):
+            classes = [eval(cls)]
         else:
-            if isinstance(cls, str):
-                cls = eval(cls)
-            objs = self.__session.query(cls)
+            classes = [cls]
+
+        objs = []
+        for cls in classes:
+            objs.extend(self.__session.query(cls).all())
+
         return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
+
 
     def new(self, obj):
         """Add obj to the current database session."""
